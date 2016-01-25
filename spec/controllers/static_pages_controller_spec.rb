f5b30f25335ad2f8cac243f20d6ff973ea3e3eaf
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-25 15:01:22 -0500

    Add test for static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,18 +2,29 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
+  context "test routes" do
 
-  describe "GET #help" do
-    it "returns http success" do
-      get :help
-      expect(response).to have_http_status(:success)
+    describe "GET #home" do
+      it "returns http success" do
+        get :home
+        expect(response).to have_http_status(:success)
+      end
     end
+
+    describe "GET #help" do
+      it "returns http success" do
+        get :help
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "GET #about" do
+      it "returns http success" do
+        get :about
+        expect(response).to have_http_status(:success)
+      end
+    end
+
   end
 
 end
