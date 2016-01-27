2d35a227a8b7c8f2e1d98a61223de04ca4228fe1
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-27 13:10:17 -0500

    Add basic styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #log_in defined in sessions_helper
       log_in user
       redirect_to user
     else
@@ -16,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
