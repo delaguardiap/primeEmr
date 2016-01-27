528f3f33a888b3876d47b1a7d4eb5dd27a2be5fe
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-27 12:17:31 -0500

    Add bootstrap for styling

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,18 @@
+module SessionsHelper
+
+  #Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #returns the current logged in user.
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #if the user is logged in it will return true, else it will return false
+  def logged_in?
+    !current_user.nil?
+  end
+
+end
